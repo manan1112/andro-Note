e1340e3c0090e7c54fb75f8ad25a589f4eff6263
manan1112 <mukhija.manan@gmail.com>
2021-11-16 10:30:06 +0530

    code commit

--- app/src/main/java/com/example/andronote/MainActivity.java
+++ app/src/main/java/com/example/andronote/MainActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+//libraries for animation
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     private static int splash_screen=7000;    //7sec
 
     //variables
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        //animation
+        //animation loading and storing it to variable
         topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
         bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
